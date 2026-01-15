6d1c91da8775a5af0e28b263892298318bb67bed
Sumeet Kumar S <xsks@valuelabsxsks.dev.secomea.com>
2026-01-15 22:23:06 +0530

    Updated Search Bus Logic

--- src/test/java/stepDefinitions/SearchBusTest.java
+++ src/test/java/stepDefinitions/SearchBusTest.java
@@ -46,7 +46,7 @@ public class SearchBusTest {
 	public void user_enters_in_the_to_input_field() throws Exception {
 
 		homepage.inputToField("Bhubaneswar");
-		homepage.selectDroppingPoint("Bermunda Bus Stand, Bhubaneswar");
+		homepage.selectDroppingPoint("Baramunda Bus Terminus, Bhubaneswar");
 		log.info("Destination city selected...");
 	}
 
@@ -89,7 +89,7 @@ public class SearchBusTest {
 		
 		
 		homepage.inputToField("Bhubaneswar");
-		homepage.selectDroppingPoint("Bermunda Bus Stand, Bhubaneswar");
+		homepage.selectDroppingPoint("Baramunda Bus Terminus, Bhubaneswar");
 		log.info("Destination city selected...");
 		
 		homepage.selectTravelDate("February");
