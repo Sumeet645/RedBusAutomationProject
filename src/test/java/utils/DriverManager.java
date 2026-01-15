e300506886e528372fad0dd1e1bc66917c206364
Sumeet645 <sumeetsatapathy24@gmail.com>
2026-01-15 12:56:52 +0530

    Enhance ChromeDriver options for CI environments
    Updated ChromeDriver setup to include additional options for headless execution and GPU disabling.

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -21,14 +21,15 @@ public class DriverManager {
 		ChromeOptions options = new ChromeOptions();
 
         // CI runners (GitHub Actions) need headless + flags
-        if ("true".equalsIgnoreCase(System.getenv("CI"))) {
             options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--window-size=1920,1080");
-        }
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--disable-gpu");
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--window-size=1920,1080");
         
-		driver.set(new ChromeDriver());
+        
+		driver.set(new ChromeDriver(options));
 	}
 	
 	public static WebDriver getDriver()
